e143a8edf31feb1e06d6181935036e28b7692515
Iblal <81813762+Iblal@users.noreply.github.com>
2022-04-21 17:25:12 +0100

    Created app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'film-hub';
+export class AppComponent implements OnInit { ngOnInit(): void {
+  setTimeout(() => {
+
+    this.movies = [{
+    title: 'Spider-Man',
+    releaseDate : new Date(),
+    price: 12.99  
+  }, 
+    { 
+    title: 'Moana',
+    releaseDate : new Date(),
+    price: 14.99 
+    }]
+    
+  }
+  )
 }
+  title = 'film-hub'
+  movies;
+};
